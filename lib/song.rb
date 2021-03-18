85f12fac48fabfe1b97899e15b5e55eecffc8ee6
Clayon Stewart <c.stewart89@gmail.com>
2021-03-18 18:05:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,8 @@ class Song
   
   def self.new_by_filename(name)
     parsed_name = name.split(" - ")
-    song = Song.new(parsed_name[0])
-    song.artist = Artist.find_or_create_by_name(parsed_name[1])
+    song = Song.new(parsed_name[1])
+    song.artist = Artist.find_or_create_by_name(parsed_name[0])
   end
   
   def artistname=(name)
