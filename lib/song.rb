283742e1e6a2f3a78eeeb79a4419e03a4f37422d
Clayon Stewart <c.stewart89@gmail.com>
2021-03-18 18:09:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
     parsed_name = name.split(" - ")
     print parsed_name
     song = Song.new(parsed_name[1])
-    puts song.title
+    puts song.name
     song.artist = Artist.find_or_create_by_name(parsed_name[0])
   end
   
